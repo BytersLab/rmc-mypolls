f7f10e93968e5d8df816dbe09b72a9ecc809ed2f
Devcodino <devcodino@proton.me>
2024-10-30 23:55:42 +0100

    Get Formdata

--- src/components/Modal.comp.jsx
+++ src/components/Modal.comp.jsx
@@ -1,11 +1,30 @@
 import { useState } from "react";
+import "./Modal.style.css";
 
 export function Modal() {
   const [modal, setModal] = useState(false);
+  const [formdata, setFormData] = useState({
+    title: "",
+    option1: "",
+    option2: "",
+    option3: "",
+  });
 
   const onTriggerModal = () => {
     setModal(!modal);
   };
+
+  const onInputChange = (event) => {
+    const stateProp = event.target.name;
+    const value = event.target.value;
+    setFormData({ ...formdata, [stateProp]: value });
+  };
+
+  const onCreatePoll = (event) => {
+    event.preventDefault();
+    console.log(formdata);
+  };
+
   return (
     <>
       {
@@ -14,11 +33,31 @@ export function Modal() {
             <div className="modal">
               <h3>Create Poll</h3>
               <form>
-                <input placeholder="Question" type="text" />
-                <input placeholder="Option 1" type="text" />
-                <input placeholder="Option 2" type="text" />
-                <input placeholder="Option 3" type="text" />
-                <button type="submit">Create</button>
+                <input
+                  onChange={onInputChange}
+                  placeholder="Question"
+                  type="text"
+                  name="title"
+                />
+                <input
+                  onChange={onInputChange}
+                  placeholder="Option 1"
+                  type="text"
+                  name="option1"
+                />
+                <input
+                  onChange={onInputChange}
+                  placeholder="Option 2"
+                  type="text"
+                  name="option2"
+                />
+                <input
+                  onChange={onInputChange}
+                  placeholder="Option 3"
+                  type="text"
+                  name="option3"
+                />
+                <button onClick={onCreatePoll}>Create</button>
               </form>
               <button className="modal-close" onClick={onTriggerModal}>
                 X
