7b3536b263444965b789923227b463e84ccbf173
Devcodino <devcodino@proton.me>
2024-10-30 22:51:45 +0100

    Loading Data from local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,28 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import { Poll } from "./components/Poll.comp.jsx";
 
-const dataArray = [
-  {
-    title: "Whats Movie you want to see?",
-    color: "#ffffff",
-    options: [
-      {
-        name: "Spiderman 1",
-        value: 1,
-      },
-      {
-        name: "Harry Potter 5",
-        value: 1,
-      },
-      {
-        name: "Herr der Ringe 2",
-        value: 1,
-      },
-    ],
-  },
-  {
-    title: "Whats Book want to read?",
-    color: "#ffffff",
-    options: [
-      {
-        name: "Book 1",
-        value: 1,
-      },
-      {
-        name: "Book 2",
-        value: 1,
-      },
-      {
-        name: "Book 3",
-        value: 1,
-      },
-    ],
-  },
-];
-
 function App() {
+  const [dataArray, setDataArray] = useState([]);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("dataArray"));
+    setDataArray(data);
+  }, []);
+
   return (
     <>
       <h1>MyPoll</h1>
-      <div className="pollcontainer">
-        {dataArray.map((item, index) => {
-          return <Poll key={index} dataArray={item} color={"#05def3"} />;
-        })}
-      </div>
+
+      {dataArray.length ? (
+        <div className="pollcontainer">
+          {dataArray.map((item, index) => {
+            return <Poll key={index} dataArray={item} color={"#05def3"} />;
+          })}
+        </div>
+      ) : (
+        "No poll available, create one!"
+      )}
     </>
   );
 }
