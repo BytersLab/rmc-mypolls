602d729788791660596ec2ff243dc802bd1c64b0
Devcodino <devcodino@proton.me>
2024-10-30 19:45:14 +0100

    Add poll change functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { Bar, BarChart, ResponsiveContainer, XAxis } from "recharts";
+import { useState } from "react";
+
 import "./App.css";
 
-const data = [
+const dataArray = [
   {
     name: "Spiderman 1",
     value: 1,
@@ -17,17 +19,36 @@ const data = [
 ];
 
 function App() {
+  const [data, setData] = useState(dataArray);
+
+  const onPollChange = (event) => {
+    const objName = event.target.innerText;
+
+    const newData = data.filter((item) => {
+      if (item.name !== objName) {
+        return item;
+      }
+      return { ...item, value: item.value++ };
+    });
+
+    setData(newData);
+  };
+
   return (
     <>
       <h1>Hello MyPolls</h1>
       <ResponsiveContainer width={"100%"} height={500}>
-        <BarChart data={data} margin={{ bottom: 120, right: 20 }}>
+        <BarChart data={data} margin={{ bottom: 120 }}>
           <Bar dataKey="value" fill="#8884d8" />
           <XAxis dataKey={"name"} interval={0} angle={-10} dy={32} />
         </BarChart>
       </ResponsiveContainer>
       {data.map((item, index) => {
-        return <button key={index}>{item.name}</button>;
+        return (
+          <button key={index} onClick={onPollChange}>
+            {item.name}
+          </button>
+        );
       })}
     </>
   );
