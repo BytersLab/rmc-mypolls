8823bb16231a396e5d1d91de66918cb1f01f4bc6
Devcodino <devcodino@proton.me>
2024-10-30 21:57:47 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,40 @@ import { Poll } from "./components/Poll.comp.jsx";
 
 const dataArray = [
   {
-    name: "Spiderman 1",
-    value: 1,
+    title: "Whats Movie you want to see?",
+    color: "#ffffff",
+    options: [
+      {
+        name: "Spiderman 1",
+        value: 1,
+      },
+      {
+        name: "Harry Potter 5",
+        value: 1,
+      },
+      {
+        name: "Herr der Ringe 2",
+        value: 1,
+      },
+    ],
   },
   {
-    name: "Harry Potter 5",
-    value: 1,
-  },
-  {
-    name: "Herr der Ringe 2",
-    value: 1,
+    title: "Whats Book want to read?",
+    color: "#ffffff",
+    options: [
+      {
+        name: "Book 1",
+        value: 1,
+      },
+      {
+        name: "Book 2",
+        value: 1,
+      },
+      {
+        name: "Book 3",
+        value: 1,
+      },
+    ],
   },
 ];
 
@@ -21,7 +45,9 @@ function App() {
     <>
       <h1>MyPoll</h1>
       <div className="pollcontainer">
-        <Poll dataArray={dataArray} color={"#05def3"} />
+        {dataArray.map((item, index) => {
+          return <Poll key={index} dataArray={item} color={"#05def3"} />;
+        })}
       </div>
     </>
   );
--- src/components/Poll.comp.jsx
+++ src/components/Poll.comp.jsx
@@ -2,43 +2,43 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 import { Bar, BarChart, XAxis } from "recharts";
 
-export function Poll({ dataArray, color }) {
+export function Poll({ dataArray }) {
   const [data, setData] = useState(dataArray);
   const [pollStatus, setPollStatus] = useState(0);
 
   const onPollChange = (event) => {
     const objName = event.target.innerText;
 
-    const newData = data.filter((item) => {
+    const newData = data.options.filter((item) => {
       if (item.name !== objName) {
         return item;
       }
       return { ...item, value: item.value++ };
     });
-    setData(newData);
+    setData({ ...data, options: newData });
     setPollStatus(1);
   };
 
   return (
     <>
       <div className="poll">
-        <h2 className="poll-title">Whats Movie you want to see?</h2>
+        <h2 className="poll-title">{data.title}</h2>
         {
           {
             1: (
               <BarChart
                 width={300}
                 height={300}
-                data={data}
+                data={data.options}
                 margin={{ bottom: 48 }}
               >
-                <Bar dataKey="value" fill={color} />
+                <Bar dataKey="value" fill={data.color} />
                 <XAxis dataKey={"name"} interval={0} angle={-10} dy={32} />
               </BarChart>
             ),
             0: (
               <div className="poll-button-container">
-                {data.map((item, index) => {
+                {data.options.map((item, index) => {
                   return (
                     <button key={index} onClick={onPollChange}>
                       {item.name}
@@ -55,6 +55,5 @@ export function Poll({ dataArray, color }) {
 }
 
 Poll.propTypes = {
-  dataArray: PropTypes.array,
-  color: PropTypes.string,
+  dataArray: PropTypes.object,
 };
